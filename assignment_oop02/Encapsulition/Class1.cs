465d2b42f39c98d66947281bb9b84cec8a58c443
moomen mohammmed <monm73530@gmail.com>
2024-12-30 20:45:18 +0200

    Encapsulation

---
+++ assignment_oop02/Encapsulition/Class1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_oop02.Encapsulition
+{
+    internal struct Employee
+    {
+        #region Encapsulation
+
+        #region Attributes
+
+        public int Id;
+        private string Name;
+        public decimal salary;
+
+        #endregion
+
+        #region Constructors
+        public Employee(int id, string name, decimal _salary)
+        {
+            Id = id;
+            Name = name;
+            salary = _salary;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+
+        {
+
+            return $"id: {Id}\nName: {Name}\nsalary: {salary:c}";
+
+        }
+        #endregion
+
+
+        #region GEt-SET
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+
+        {
+
+            Name = name.Length > 5 ? name.Substring(0, 5) : name;
+
+        }
+        #endregion
+
+        #region Property
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value > 5000 ? 5000 : value;
+            }
+
+        }
+
+        public int Age { get; set; }
+
+        //private decimal deduction;
+        public decimal Deduction
+        {
+            get { return salary * 0.2m; }
+        }
+
+
+
+        #endregion 
+        #endregion
+
+
+
+
+
+
+
+    }
+}
---
+++ assignment_oop02/Program.cs
@@ -0,0 +1,32 @@
+﻿using assignment_oop02.Encapsulition;
+
+namespace assignment_oop02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee(10, "Moamen", 4000);
+
+
+
+            Console.WriteLine(employee.ToString());
+
+            //employee.Id = 100;
+
+            //Console.WriteLine(employee.Id);
+
+
+            //employee.SetName("Ahmed");
+
+            //Console.WriteLine(employee.GetName());
+
+            employee.Salary = 6000;
+
+            Console.WriteLine(employee.Salary);
+            Console.WriteLine(employee.Deduction);
+
+        }
+    }
+}

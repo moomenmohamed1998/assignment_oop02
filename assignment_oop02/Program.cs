0514448ba7431d8fb708e16fbfe102cbd80b66aa
moomen mohammmed <monm73530@gmail.com>
2024-12-30 22:10:06 +0200

    Demo

--- assignment_oop02/Program.cs
+++ assignment_oop02/Program.cs
@@ -7,11 +7,12 @@ namespace assignment_oop02
         static void Main(string[] args)
         {
 
-            Employee employee = new Employee(10, "Moamen", 4000);
+            #region Encapsulatoin
+            //Employee employee = new Employee(10, "Moamen", 4000);
 
 
 
-            Console.WriteLine(employee.ToString());
+            //Console.WriteLine(employee.ToString());
 
             //employee.Id = 100;
 
@@ -22,10 +23,35 @@ namespace assignment_oop02
 
             //Console.WriteLine(employee.GetName());
 
-            employee.Salary = 6000;
+            //employee.Salary = 6000;
 
-            Console.WriteLine(employee.Salary);
-            Console.WriteLine(employee.Deduction);
+            //Console.WriteLine(employee.Salary);
+            //Console.WriteLine(employee.Deduction); 
+            #endregion
+
+            phonebook Note = new phonebook(3);
+            Note.Addpersone(0, "Ahmed", 123);
+            Note.Addpersone(1, "Moamen", 546);
+            Note.Addpersone(2, "Marwan", 333);
+
+
+            //int Number = Note.GetNumber("Moamen");
+            //Console.WriteLine(Number == -1 ? "Note Found" : Number);
+            //Note.SetNumber("Mona", 999);
+
+            //Note["Moamen"] = 999;
+            //Console.WriteLine(Note["Moamen"]);
+
+            for (int i = 0; i < Note._Size; i++)
+            {
+
+                Console.WriteLine(Note[i]);
+            }
+
+            string Name = "Moamen";
+            Console.WriteLine(Name[0]);
+
+
 
         }
     }
